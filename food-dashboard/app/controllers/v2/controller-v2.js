f6284ad776b118e8d6ddfe28a29fe833259e20b9
sonhuynhdev <huynhthaison.dev@gmail.com>
2023-10-16 15:06:37 +0700

    update

--- food-dashboard/app/controllers/v2/controller-v2.js
+++ food-dashboard/app/controllers/v2/controller-v2.js
@@ -1,7 +1,5 @@
 export let renderFoodList = (foodArr) => {
   let contentHTML = "";
-  console.log("♣ ~ renderFoodList ~ renderFoodList:", foodArr);
-
   foodArr.forEach((foodArr) => {
     let { ma, ten, gia, khuyenMai, hinhAnh, moTa, loai, tinhTrang } = foodArr;
     let trString = `<tr>
@@ -14,10 +12,32 @@ export let renderFoodList = (foodArr) => {
                           <td>${tinhTrang}</td>
                           <td>
                             <button class="btn btn-danger" onclick=deleteFood(${ma}) >Xoá</button>
+                            <button class="btn btn-primary" onclick=editFood(${ma})>Edit</button>
                           </td>
                       </tr>`;
     contentHTML = contentHTML + trString;
   });
 
   document.getElementById("tbodyFood").innerHTML = contentHTML;
+};
+
+// tạo biến cho chay ,mặn
+const monChay = "loai1";
+const monMan = "loai2";
+const chay = true;
+const man = false;
+const conMon = true;
+const tinhTrangCon = "1";
+const tinhTrangHet = "0";
+export let showDataForm = (food) => {
+  let { ma, ten, loai, gia, khuyenMai, tinhTrang, hinhAnh, moTa } = food;
+  document.getElementById("foodID").value = ma;
+  document.getElementById("tenMon").value = ten;
+  document.getElementById("loai").value = loai == chay ? monChay : monMan;
+  document.getElementById("giaMon").value = gia;
+  document.getElementById("khuyenMai").value = khuyenMai;
+  document.getElementById("tinhTrang").value =
+    tinhTrang == conMon ? tinhTrangCon : tinhTrangHet;
+  document.getElementById("hinhMon").value = hinhAnh;
+  document.getElementById("moTa").value = moTa;
 };
--- food-dashboard/app/controllers/v2/main-v2.js
+++ food-dashboard/app/controllers/v2/main-v2.js
@@ -1,6 +1,6 @@
 import { https } from "../../../service/service.js";
 import { layThongtinTuForm } from "../v1/controller-v1.js";
-import { renderFoodList } from "./controller-v2.js";
+import { renderFoodList, showDataForm } from "./controller-v2.js";
 let fetchFoodList = () => {
   https
     .get("/food")
@@ -31,10 +31,36 @@ window.addFood = () => {
   https
     .post("/food", food)
     .then((res) => {
+      fetchFoodList();
       $("#myModal").modal("hide");
       console.log(res);
     })
     .catch(function (err) {
+      console.log(err);
+    });
+};
+
+window.editFood = (id) => {
+  $("#exampleModal").modal("show");
+  https
+    .get(`/food/${id}`)
+    .then((res) => {
+      showDataForm(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.updateFood = () => {
+  let food = layThongtinTuForm();
+  https
+    .put(`/food/${food.ma}`, food)
+    .then((res) => {
+      $("#exampleModal").modal("hide");
+      fetchFoodList();
+    })
+    .catch((err) => {
       console.log(err);
     });
 };
